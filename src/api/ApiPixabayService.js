7edc3eefe98eb5cc42ced7b95575ec861aa9eaa8
Vlad Pshenichnyi <PshenichnyiVlad@gmail.com>
2022-02-19 23:50:45 +0200

    pagination done
    Пагинация через кнопку догрузки готова, лайтбокс подключен осталось чуть чуть

--- src/api/ApiPixabayService.js
+++ src/api/ApiPixabayService.js
@@ -1,14 +1,19 @@
+import axios from 'axios';
+
 const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '/?key=25756653-ca7b891a55f16e964dd1f6216';
-const OPTIONS_SEARCH = 'image_type=photo&orientation=horizontal&safesearch=false&per_page=40'
+const OPTIONS_SEARCH = 'image_type=photo&orientation=horizontal&safesearch=true&per_page=40'
 
 export default class FetchApiPictures { 
     constructor() { 
         this.searchPicture = '';
-        this.page = 1
+        this.page = 1;
+        this.totalPictures = 0;
+        this.stillHavePicture = 0;
+        this.parePage = 0;
     };
 
-    fetchPicture() {        
+    async fetchPicture() {        
         const url = `${BASE_URL}${API_KEY}&q=${this.searchPicture}&${OPTIONS_SEARCH}&page=${this.page}`
         return fetch(url).then(response => {
             if (!response.ok) {
@@ -17,6 +22,11 @@ export default class FetchApiPictures {
             return response.json();
         }).then(data => {
             this.incrementPage()
+            this.stillHave()
+            // this.totalPicture(data.total)
+            // this.stillHave()
+            // console.log(this.stillHavePicture);
+            // console.log(this.stillHavePicture - this.parePage)
             return data;               
         })   
     }
@@ -27,6 +37,21 @@ export default class FetchApiPictures {
 
     resetPage() {
         this.page = 1;
+    }
+
+    totalPicture(data, page) { 
+        this.totalPictures = data;
+        this.parePage = page
+        // console.log(this.totalPictures)
+        // console.log(this.parePage)
+    }
+
+    stillHave() {
+        if (this.totalPictures >= 40) {
+            this.totalPictures -= this.parePage
+        } if (this.totalPictures < 40 )  { 
+            this.parePage = this.totalPictures
+        }    
     }
 
     get query() { 
--- src/index.js
+++ src/index.js
@@ -13,6 +13,15 @@ const refs = {
     loadMoreBtn: document.querySelector('.load-more')
 }
 
+const { height: cardHeight } = document
+    .querySelector('.gallery')
+    // .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+    });
+
 const fetchApiPictures = new FetchApiPictures();
 
 const lightbox = new SimpleLightbox('.gallery a', {
@@ -20,24 +29,17 @@ const lightbox = new SimpleLightbox('.gallery a', {
     captionDelay: 300,
 });
 
-// window.scrollBy({
-//     top: cardHeight * 2,
-//     behavior: 'smooth',
-//     });
-// const { height: cardHeight } = document
-//     .querySelector('.gallery')
-//     .firstElementChild.getBoundingClientRect();
-
 refs.inputForm.focus()
 refs.searchForm.addEventListener('submit', onSearchPictures)
 refs.loadMoreBtn.addEventListener('click', onLoadMore)
 
 function onSearchPictures(e) {
     e.preventDefault()
+    window.scrollTo(0,0)
 
-    fetchApiPictures.query = e.currentTarget.elements.searchQuery.value;
+    fetchApiPictures.query = e.currentTarget.elements.searchQuery.value.trim();
     const searchPicture = fetchApiPictures.query
-
+    
     if (searchPicture === '') {
         return Notiflix.Notify.failure('Введите что нибудь');
     }
@@ -47,24 +49,42 @@ function onSearchPictures(e) {
         if (data.total === 0) { 
             Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
         } else {
-            Notiflix.Notify.success(`Мы нашли ${data.total} изображений по запросу ${searchPicture}`)
-            clearPicturesGallery()
-            createGallery(data.hits)
+            Notiflix.Notify.success(`Мы нашли ${data.total} изображений по запросу ${searchPicture}`);
         }
+        fetchApiPictures.totalPicture(data.total, data.hits.length)
+        fetchApiPictures.stillHave()
+        loadMoreShowBtn()
+        clearPicturesGallery();
+        createGallery(data.hits);
+        lightbox.refresh();
     })
 }
 
 function onLoadMore() { 
     fetchApiPictures.fetchPicture().then(data => {        
         createGallery(data.hits)
+        lightbox.refresh();
+        if (data.hits.length < 40) {
+            refs.loadMoreBtn.classList.remove('active')         
+            return Notiflix.Notify.success(`Эти были последние =(`)
+        } 
+        refs.loadMoreBtn.textContent = `Показать еще ${fetchApiPictures.parePage} из ${fetchApiPictures.totalPictures}`
     })
-
+    fetchApiPictures.stillHave()
 }
-    
+
 function createGallery(hits) { 
     refs.picturesGallery.insertAdjacentHTML('beforeend', pictureListTmp(hits))
 }
 
 function clearPicturesGallery() { 
     refs.picturesGallery.innerHTML = '';
+}
+
+function loadMoreShowBtn() { 
+    if (fetchApiPictures.totalPictures > 40) { 
+        refs.loadMoreBtn.classList.add('active')
+    }
+    refs.loadMoreBtn.textContent = `Показать еще ${fetchApiPictures.parePage} из ${fetchApiPictures.totalPictures}`
+    
 }
